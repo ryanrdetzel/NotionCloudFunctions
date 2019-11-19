52458a2cd5eebc77369e41b035cd4282a0b7700f
root <root@li366-86.members.linode.com>
2019-11-19 15:38:57 -0500

    basic api key auth to stop spam

--- quickadd/quickadd.py
+++ quickadd/quickadd.py
@@ -7,18 +7,24 @@ from notion.block import TextBlock
 
 token = os.environ.get('token', 'Missing notion login token')
 page_url = os.environ.get('page', 'Missing notion page url')
+api_key = os.environ.get('api_key', 'Missing api key')
 
 client = NotionClient(token_v2=token)
 
 def quickadd(request):
-  title = 'Untitled'
   args = request.args.to_dict(flat=True)
-  if args and 'title' in args:
-    title = args.get('title')
+  if not args:
+    return 'missing args', 500
+  if 'api' not in args:
+    return 'missing api key', 500
+  if args.get('api') != api_key:
+    return 'api key is incorrect'
+
+  title = args.get('title', 'Untitled')
 
   parent_page = client.get_block(page_url)
   page = parent_page.children.add_new(
     TextBlock,
     title=title,
   )
-  return 'done'
+  return page.id
